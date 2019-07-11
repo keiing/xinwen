93465fe008260279ffddc4f92876d0b0bcdab1d7
keiing <49868852+keiing@users.noreply.github.com>
2019-07-11 22:30:27 +0800

    添加了vue框架

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -20,12 +20,12 @@ server.use(multiparty({
 server.use(
   //cors方法解决跨域
   cors({
-//    origin: [
-//      "http://127.0.0.1:5500",
-//      "http://127.0.0.1:8080",
-//      "http://localhost:5500",
-//      "http://localhost:8000"
-//    ],
+    origin: [
+      "http://127.0.0.1:5500",
+      "http://127.0.0.1:8080",
+      "http://localhost:5500",
+      "http://localhost:8000"
+    ],
     credentials: true
   })
 );
